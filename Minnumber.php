3b1236137671cf1c75ee818affb61812ea425a74
sunhikun <44692329+sunhikun@users.noreply.github.com>
2019-03-22 08:46:54 +0800

    Add files via upload

---
+++ Minnumber.php
@@ -0,0 +1,18 @@
+<?php 
+namespace app\week\controller;
+
+use think\Controller;
+
+class Minnumber extends Controller
+{
+	public function number()
+	{
+		$numbers = [1,2,12];
+
+		for ($i=0; $i < 1222; $i++) { 
+			if($numbers = 1122){
+				break;
+			}
+		}
+	}
+}
